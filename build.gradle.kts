7b07ef6b6be13731fee793b09a851f7748f5a252
gopinath <gopinath@myeglu.com>
2021-08-16 12:19:28 +0530

    Added shadowjar plugin for creating fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     kotlin("jvm") version "1.5.30-M1"
+    id("com.github.johnrengelman.shadow") version ("7.0.0")
 }
 
 group = "com.wizn"
@@ -10,6 +11,7 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
@@ -18,4 +20,17 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+            "Implementation-Version" to project.version))
+    }
+}
+
+tasks.shadowJar {
+    manifest {
+        attributes(mapOf("Main-Class" to "server.MainKt"))
+    }
 }
--- src/main/kotlin/server/Main.kt
+++ src/main/kotlin/server/Main.kt
@@ -1,10 +1,11 @@
 package server
 
+import java.lang.IllegalStateException
 import kotlin.concurrent.thread
 
 val TCP_SERVER_PORT = System.getenv("TCP_SERVER_PORT")?.toInt() ?: 10001
-val UPSTREAM_UDP_ADDRESS = System.getenv("UPSTREAM_UDP_ADDRESS") ?: "staging.myeglu.com"
-val UPSTREAM_UDP_PORT = System.getenv("UPSTREAM_UDP_PORT")?.toInt() ?: 8015
+val UPSTREAM_UDP_ADDRESS = System.getenv("UPSTREAM_UDP_ADDRESS") ?: throw IllegalStateException("Upstream address undefined")
+val UPSTREAM_UDP_PORT = System.getenv("UPSTREAM_UDP_PORT")?.toInt() ?: throw IllegalStateException("Upstream port undefined")
 
 
 fun main() {
